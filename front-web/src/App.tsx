e9c6d0158cecde28e9239438d298241565b2f083
Gleyson Miranda Souza <gleydson.m.s.1993@gmail.com>
2021-04-01 21:33:37 -0300

    customizing the bootstrap

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
 
 const App = () => {
-    return <h1>Hello App</h1>
+    
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
